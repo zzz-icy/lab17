710569d0345cc6b706b5643577eb76c238ffa232
Han Zhang <eveline1129@163.com>
2018-10-28 17:38:27 -0500

    added individual info on people page

--- src/components/PeoplePage.js
+++ src/components/PeoplePage.js
@@ -2,10 +2,54 @@ import React from 'react';
 import PeopleExpansionPanel from './PeopleExpansionPanel';
 import quans from '../images/peoplePhoto/quans.png';
 import yejb from '../images/peoplePhoto/yejb.png';
+import gong from '../images/peoplePhoto/gong.png';
+import liu from '../images/peoplePhoto/liu.png';
+import kentl from '../images/peoplePhoto/kentl.png';
+import axiang from '../images/peoplePhoto/axiang.png';
+import guo from '../images/peoplePhoto/guo.jpg';
+
+
+
 
 const PeoplePage = () => (
     <div className="page-header">
         <div className="content-container">
+
+            <PeopleExpansionPanel
+                img={yejb}
+                name="Ye, Jingbo"
+                title="Professor of Experimental Physics"
+                mobile="(214)768-2114"
+                email="yejb@smu.edu"
+            />
+            <PeopleExpansionPanel
+                img={gong}
+                name="Gong, Datao"
+                title="Professor of Experimental Physics"
+                mobile="(214)768-2114"
+                email="yejb@smu.edu"
+            />
+            <PeopleExpansionPanel
+                img={liu}
+                name="Liu, Tiankuan (Andy)"
+                title="Professor of Experimental Physics"
+                mobile="(214)768-2114"
+                email="yejb@smu.edu"
+            />
+            <PeopleExpansionPanel
+                img={kentl}
+                name="Liu, Chonghan (Kent)"
+                title="Professor of Experimental Physics"
+                mobile="(214)768-2114"
+                email="yejb@smu.edu"
+            />
+            <PeopleExpansionPanel
+                img={axiang}
+                name="Xiang, Annie"
+                title="Professor of Experimental Physics"
+                mobile="(214)768-2114"
+                email="yejb@smu.edu"
+            />
             <PeopleExpansionPanel
                 img={quans}
                 name="Sun, Quan"
@@ -14,8 +58,8 @@ const PeoplePage = () => (
                 email="quans@smu.edu"
             />
             <PeopleExpansionPanel
-                img={yejb}
-                name="Ye, Jingbo"
+                img={guo}
+                name="Guo, Di"
                 title="Professor of Experimental Physics"
                 mobile="(214)768-2114"
                 email="yejb@smu.edu"
